ab2b26d6e7f0efddc9ee2f8dfe45e4a8582df15e
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-16 16:47:43 -0400

    update cors policy access for hosting site

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://localhost:3000"],
+    origin: ["https://aqi-checker.cyclic.app/", "https://busy-fish-gown.cyclic.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
