3e2864e9eac64dce2605a868899883f9612ed3a7
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-18 10:26:26 -0400

    update centering of span in citysearch, add sendAllEmail to controller and route  for cyclic hosting cron

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
-const { Signup, Login, EditTrackedLocations, UpdateSendEmail } = require('../controllers/auth')
+const { Signup, Login, EditTrackedLocations, UpdateSendEmail, SendAllEmails } = require('../controllers/auth')
 const {userVerification} = require('../middleware/authentication')
 const router = require('express').Router()
 
 router.post('/signup', Signup)
 router.post('/login', Login)
 router.post('/', userVerification)
+router.post('/', SendAllEmails)
 router.put('/editTrackedLocations', EditTrackedLocations)
 router.put('/updateSendEmail', UpdateSendEmail)
 
