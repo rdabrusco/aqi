7bc37a6cd62b69f5995b41cce9b09a0b68249f7b
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-18 09:07:17 -0400

    add catchall route to index.js

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+
 
 app.use("/", authRoute);
 
